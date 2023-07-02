4c63f36a3def8bdecc5ab041818acea6b8005397
Rishav045 <20bds045@iiitdwd.ac.in>
2023-07-02 18:31:32 +0530

    adding home page

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const app = express();
 app.get('/',(req,res)=>{
     res.send("Hello there")
 })
+app.get('/home',(req,res)=>{
+    res.send("This is the home page")
+})
 app.listen(3000,()=>{
     console.log("Server is listening on port 3000");
 })
